c91a1ef30daa2fb0211684dffe19c745679b6c9d
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2023-02-19 17:12:53 +0100

    refactored pythagore - hopefully easier to understand
    - use latest d3 version 7.8.2 (no change incurred)
    - smarter use of transforms:
    * all elements start with a neutral transform
    * use composite transform to translate then rotate
    * move the b2 label when its square gets moved up
    - shorter code
    * remove dead code keydown
    * use helper to interpolate transforms

--- samples/44-pythagore/pythagore.js
+++ samples/44-pythagore/pythagore.js
@@ -1,15 +1,14 @@
 window.addEventListener('load',
 
-	() => {
-    let a = 200, b = 550
-    let delay = 600
+  () => {
+    let [a, b] = [100, 300]
+    let [delay_geom, delay_text] = [600, 1200]
 
-    let svg = d3.select('#container_pythagore')
-		  .append('svg')
-		  // would need to compute the border + padding for svg
-      //.attr('width', function(){FOO=this; console.log(this); return a+b;})
-      .attr('width', a+b+8)
-      .attr('height', a+b+8)
+    let svg = d3.select('#container-pythagore')
+      .append('svg')
+      // would need to compute the border + padding for svg
+      .attr('width', a + b + 8)
+      .attr('height', a + b + 8)
       .attr("xmlns", "http://www.w3.org/2000/svg")
 
 
@@ -17,185 +16,186 @@ window.addEventListener('load',
       .attr('class', 'background')
       .attr('x', 0)
       .attr('y', 0)
-      .attr('width', a+b)
-      .attr('height', a+b)
+      .attr('width', a + b)
+      .attr('height', a + b)
 
-    // left most
-    function t1_path() { return `M ${a} 0 l ${-a} ${b} l 0 ${-b} Z` }
-    // left internal
-    function t2_path() { return `M ${a} 0 l ${-a} ${b} l ${a} 0 Z` }
-    // lowest
-    function t3_path() { return `M ${a} 0 l ${b} ${a} l ${-b} 0 Z` }
-    // low internal
-    function t4_path() { return `M ${a} 0 l ${b} ${a} l 0 ${-a} Z` }
+    // remember the svg coordinate system has y=0 UP and y increases as we go down
+    // used when resetting things
+    const transform_neutral = `translate(0, 0) rotate(0, 0, 0)`
+    const transform_up = `translate(0, -${b}) rotate(0, 0, 0)`
+    const transform_down = `translate(0, ${a}) rotate(0, 0, 0)`
 
-    let transform_neutral = 'translate(0,0) rotate(0,0,0)'
-    let t1 = svg.append('path')
+    // left most - this one won't move
+    svg.append('path')
       .attr('id', 't1')
       .attr('class', 'triangle')
-      .attr('d', t1_path())
-      .attr('transform', transform_neutral)
+      .attr('d', `M ${a} 0 l ${-a} ${b} l 0 ${-b} Z`)
+    // .attr('transform', transform_neutral)
 
-    let t2 = svg.append('path')
+    // left internal, will turn 90° clockwise
+    svg.append('path')
       .attr('id', 't2')
-      .attr('class', 'triangle')
-      .attr('d', t2_path())
-      .attr('transform', transform_neutral)
+      .attr('class', 'triangle reset')
+      .attr('d', `M ${a} 0 l ${-a} ${b} l ${a} 0 Z`)
+    // .attr('transform', transform_neutral)
 
-    // t3 and t4 are created so that theit translate() transform is neutral once they are up
-    let transform_initial = 'translate(0,' + b + ') rotate(0,0,0)'
-    let t3 = svg.append('path')
+    // lowest - will move up, then turn 90° anti-clockwise
+    svg.append('path')
       .attr('id', 't3')
-      .attr('class', 'triangle move_up')
-      .attr('d', t3_path())
-      .attr('transform', transform_initial)
+      .attr('class', 'triangle move_up reset')
+      .attr('d', `M ${a} ${b} l ${b} ${a} l ${-b} 0 Z`)
+    // .attr('transform', transform_neutral)
 
-    let t4 = svg.append('path')
+    // low internal - will just move up with t3
+    svg.append('path')
       .attr('id', 't4')
-      .attr('class', 'triangle move_up')
-      .attr('d', t4_path())
-      .attr('transform', transform_initial)
+      .attr('class', 'triangle move_up reset')
+      .attr('d', `M ${a} ${b} l ${b} ${a} l 0 ${-a} Z`)
+    // .attr('transform', transform_neutral)
 
 
-    let text_a = svg.append('text')
-      .attr('x', a/2)
-      .attr('y', b+(a/2))
+    svg.append('text')
       .attr('id', 'a2')
+      .attr('class', 'legend a_or_b')
+      .attr('opacity', 1)
+      .attr('x', a / 2)
+      .attr('y', b + (a / 2))
       .html('<tspan>a²</tspan>')
-      .attr('class', 'legend text_before')
 
-
-    let text_b = svg.append('text')
-      .attr('x', a + (b/2))
-      .attr('y', b/2)
+    svg.append('text')
       .attr('id', 'b2')
+      .attr('class', 'legend a_or_b')
+      .attr('opacity', 1)
+      .attr('x', a + (b / 2))
+      .attr('y', b / 2)
       .html('<tspan>b²</tspan>')
-      .attr('class', 'legend text_before')
 
-
-    let text_c = svg.append('text')
-      .attr('x', (a+b)/2)
-      .attr('y', (a+b)/2)
+    svg.append('text')
+      .attr('id', 'c2')
+      .attr('class', 'legend')
+      .attr('opacity', 0)
+      .attr('x', (a + b) / 2)
+      .attr('y', (a + b) / 2)
       .html('<tspan>c²</tspan>')
-      .attr('class', 'legend text_after')
-      .attr('display', 'none')
 
 
-    let rect = svg.append('rect')
+    svg.append('rect')
       .attr('id', 'foo')
       .attr('x', 800)
       .attr('y', 800)
       .attr('width', 100)
       .attr('height', 100)
 
+    // interpolate between current transform
+    // and a specified target
+    function transform_helper(elt, target_transform) {
+      let previous_transform = elt.getAttribute("transform")
+      return d3.interpolateTransformSvg(previous_transform, target_transform)
+    }
 
     ////////////////////
     function move_up() {
-      svg.selectAll(".move_up")
+      svg.
+        selectAll(".move_up")
         .transition()
-        .duration(delay)
-        .attrTween(
-          'transform',
-          function(d, i) {
-            return d3.interpolateTransformSvg(
-              transform_initial, transform_neutral)
-          })
+        .duration(delay_geom)
+        .attrTween('transform', function (d, i) {
+          return transform_helper(this, transform_up)
+        })
+
+      // move the b square label down to end in the middle of its square
+      svg
+        .select("#b2")
+        .transition()
+        .duration(delay_geom)
+        .attrTween('transform', function (d, i) {
+          return transform_helper(this, transform_down)
+        })
     }
 
     ////////////////////
     function turn_right() {
-      svg.selectAll("#t2")
+      svg
+        .selectAll("#t2")
         .transition()
-        .duration(delay)
-        .attrTween('transform',
-          function(d, i) {
-          return d3.interpolateTransformSvg(
-            transform_neutral,
-            'translate(0,0) rotate(90,0,' + b + ')')
-          })
+        .duration(delay_geom)
+        .attrTween('transform', function (d, i) {
+          return transform_helper(this, `rotate(90, 0, ${b})`)
+        })
     }
 
     function turn_left() {
-      svg.selectAll('#t3')
+      svg
+        .selectAll('#t3')
         .transition()
-        .duration(delay)
-        .attrTween('transform',
-          function(d, i) {
-          return d3.interpolateTransformSvg(
-            transform_neutral,
-            'translate(0,0) rotate(-90,' + (a+b) + ',' + a + ')')
-          })
+        .duration(delay_geom)
+        .attrTween('transform', function (d, i) {
+          // composite transform:
+          // we first translate (which actually is its current transform)
+          // and then rotate around the point (a+b, a)
+          // counter-intuitively the order is reversed
+          return transform_helper(this, `rotate(-90, ${a + b}, ${a}) translate(0, -${b})`)
+        })
     }
 
-    function switch_texts() {
-      svg.selectAll('.text_before')
-          .transition()
-          .duration(delay)
-          .attr('display', 'none')
-      svg.selectAll('.text_after')
-          .transition()
-          .duration(delay)
-          .attr('display', 'on')
-    }
-
-    function switch_back_texts() {
-      svg.selectAll('.text_after')
+    function texts_ab_off_c_on() {
+      svg
+        .selectAll('.a_or_b')
         .transition()
-        .duration(delay)
-        .attr('display', 'none')
-      svg.selectAll('.text_before')
+        .duration(delay_text)
+        .attr('opacity', 0)
+      svg
+        .selectAll('#c2')
         .transition()
-        .duration(delay)
-        .attr('display', 'on')
+        .duration(delay_text)
+        .attr('opacity', 1)
     }
 
     function turn() {
-      console.log('turn')
       turn_right()
       turn_left()
-      switch_texts()
+      texts_ab_off_c_on()
       return true
     }
 
     ////////////////////
     function reset() {
-      switch_back_texts()
-      svg.select('#t2')
+      svg.selectAll('.reset')
         .transition()
-        .duration(delay)
-        .attrTween('transform',
-          function(d, i) {
-          let previous_transform = this.getAttribute("transform")
-          return d3.interpolateTransformSvg(
-            previous_transform, transform_neutral)
-          })
+        .duration(delay_geom)
+        .attrTween('transform', function (d, i) {
+          return transform_helper(this, transform_neutral)
+        })
 
-      svg.select('#t3')
+      svg
+        .select('#a2')
         .transition()
-        .duration(delay)
-        .attrTween('transform',
-          function(d, i) {
-          let previous_transform = this.getAttribute("transform")
-          return d3.interpolateTransformSvg(
-            previous_transform, transform_initial)
-          })
-
-      svg.select('#t4')
+        .duration(delay_text)
+        .attr('opacity', 1)
+      svg
+        .select('#c2')
         .transition()
-        .duration(delay)
-        .attrTween('transform',
-          function(d, i) {
-          let previous_transform = this.getAttribute("transform")
-          return d3.interpolateTransformSvg(
-            previous_transform, transform_initial)
-          })
+        .duration(delay_text)
+        .attr('opacity', 0)
+      // unfortunately if we do these 2 things as part of more general select's
+      // only only one gets done
+      svg
+        .select('#b2')
+        .transition()
+        .duration(delay_text)
+        .attr('opacity', 1)
+        .duration(delay_geom)
+        .attrTween('transform', function (d, i) {
+          return transform_helper(this, transform_neutral)
+        })
 
     }
 
     ////////////////////
+    // will cycle: 0=start, 1=moved, 2=turned, and back to 0
     let step = 0
 
-    function go() {
+    function next_step() {
       let go = document.getElementById('go')
       if (step == 0) {
         move_up()
@@ -218,22 +218,6 @@ window.addEventListener('load',
       }
     }
 
-    function keydown(event) {
-		//console.log("keydown", event.which)
-		switch (event.which) {
-			case 40: // down
-			case 39: // right
-			case 34: // page down
-			case 37: // left
-			case 38: // up
-			case 33: // page up
-				go()
-				event.preventDefault()
-				break
-		}
-	}
-
-  document.addEventListener('keypress', (event)=>go())
-  document.addEventListener('keydown', keydown)
-	document.getElementById("go").addEventListener('click', go)
-})
+    document.addEventListener('keypress', (event) => next_step())
+    document.getElementById("go").addEventListener('click', next_step)
+  })
